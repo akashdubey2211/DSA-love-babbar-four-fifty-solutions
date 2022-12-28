011669aeb791c47edd3d3339bea6cf0338eb6bb1
akashdubey2211 <dubeyakash145@gmail.com>
2022-12-28 19:24:08 +0530

    Question-6: Sort 0,1,2 of given array without using any sorting algo.

--- src/sort012.java
+++ src/sort012.java
@@ -2,12 +2,12 @@ public class sort012 {
     public static void main(String[] args) {
         int arr[] = {2,1,0,2,1,0,0};
         int n = arr.length;
-        sortArr(arr,n);
+        sort012array(arr,n);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] +" ");
         }
     }
-    public static void sortArr(int arr[],int n) {
+    public static void sort012array(int arr[],int n) {
         int low = 0;
         int high = arr.length -1;
         int mid = 0 ;
