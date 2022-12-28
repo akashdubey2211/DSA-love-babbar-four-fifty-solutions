2416d163c87cf0d1d23e3e00974ee3c2bfd9b491
akashdubey2211 <dubeyakash145@gmail.com>
2022-12-28 19:21:26 +0530

    move zeros to first of array.

---
+++ src/movezerostofirst.java
@@ -0,0 +1,26 @@
+public class movezerostofirst {
+    public static void main(String[] args) {
+        int arr[] = {2,3,4,0,2,0};
+        int n  = arr.length;
+        sortZeros(arr, n);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+    public static  void sortZeros(int arr[], int n) {
+        int j = 0;
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]<=0){
+                if (arr[i] != arr[j]) {
+                    temp = arr[i];
+                    arr[i]= arr[j];
+                    arr[j] = temp;
+
+                }
+                j++;
+            }
+        }
+    }
+}
