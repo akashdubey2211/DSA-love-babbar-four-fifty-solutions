ab6366ccd722d3b6780f1bfd5b8890329b7852d4
akashdubey2211 <dubeyakash145@gmail.com>
2022-12-28 19:05:54 +0530

    sort arr 0,1,2 without using any sorting algo.

--- src/sort012.java
+++ src/sort012.java
@@ -3,8 +3,37 @@ public class sort012 {
         int arr[] = {2,1,0,2,1,0,0};
         int n = arr.length;
         sortArr(arr,n);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] +" ");
+        }
     }
     public static void sortArr(int arr[],int n) {
-        
+        int low = 0;
+        int high = arr.length -1;
+        int mid = 0 ;
+        int temp = 0;
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                
+                    temp = arr[low];
+                    arr[low]  = arr[mid];
+                    arr[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                     temp = arr[mid];
+                     arr[mid] = arr[high];
+                     arr[high] = temp;
+                     high--;
+                     break;
+
+            }
+            
+        }
     }
 }
